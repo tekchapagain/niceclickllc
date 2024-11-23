6fb04c593cbed3a4f615b5b53ac7bd6a7b7ba345
Tek Chapagain <tekchapagain1@gmail.com>
2024-11-23 19:44:32 +0545

    Added production url

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production : false,
-    apiUrl : 'http://localhost:8000/api/v1/'
+    production : true,
+    apiUrl : 'https://django-api-ebon.vercel.app/api/v1/'
 }
